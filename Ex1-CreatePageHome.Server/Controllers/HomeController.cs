66cd56b0fa9f51b61da7bfbd14438ffe36ce5b00
Mehdi Soheili <MEHDISOHEILI2018@GMAIL.COM>
2024-09-06 19:30:25 +0330

    Add project files.

---
+++ Ex1-CreatePageHome.Server/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ex1_CreatePageHome.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public List<string> Get()
+        {
+            return new List<string>() { "Page Home " };
+        }
+    }
+}
---
+++ ex1-createpagehome.client/src/App.tsx
@@ -0,0 +1,26 @@
+﻿import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+    const [result, sethome] = useState<Array<string>>();
+
+    useEffect(() => {
+        homeData();
+    }, []);
+
+    const contents = result?.map(x => x);
+    return (
+        <div>
+            <h1 id="tableLabel">{contents}</h1>
+        </div>
+    );
+
+    async function homeData() {
+        console.log("homeData");
+        const response = await fetch('/home'); 
+        const data = await response.json();
+            sethome(data);
+    }
+}
+
+export default App;
